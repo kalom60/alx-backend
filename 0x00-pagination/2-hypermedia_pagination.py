3d9ca3b2fe62da5b0dee1ad9dca73a6b0516f98a
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-07-10 14:47:46 +0300

    Hypermedia pagination

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+"""Simple Pagination"""
+import csv
+import math
+from typing import List, Tuple, Dict
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+     return a tuple of size two containing a start index and
+     an end index corresponding to the range of indexes
+    """
+    return ((page * page_size) - page_size, (page * page_size))
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        return the appropriate page of the datase
+        If the input arguments are out of range for the dataset,
+        an empty list should be returned
+        """
+        assert isinstance(page, int) and isinstance(page_size, int)
+        assert page > 0 and page_size > 0
+        all_names = self.dataset()
+        start, end = index_range(page, page_size)
+        if start > len(all_names):
+            return []
+        return all_names[start:end]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        """ returns a dictionary """
+        data = self.get_page(page, page_size)
+        next_page = prev_page = None
+        if (page * page_size) < len(self.__dataset):
+            next_page = page
+            next_page += 1
+        if page > 1:
+            prev_page = page
+            prev_page -= 1
+        return {
+                'page_size': page_size,
+                'page': page,
+                'data': data,
+                'next_page': next_page,
+                'prev_page': prev_page,
+                'total_pages': math.ceil(len(self.__dataset) / page_size)
+                }
