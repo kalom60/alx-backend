92a513f77791ccaf7dc151728081a72a4ce96ef1
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-07-20 00:20:09 +0300

    use '' instead of None in get_locale

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -45,7 +45,7 @@ def before_request():
 @babel.localeselector
 def get_locale():
     """create a locale from request"""
-    locale = request.args.get('locale', None)
+    locale = request.args.get('locale', '')
     if locale in app.config['LANGUAGES']:
         return locale
     return request.accept_languages.best_match(app.config["LANGUAGES"])
