a95e0236806002d30668466820b02a5144283dc4
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-07-20 01:10:00 +0300

    fix error

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python3
-"""create a basic flask app"""
-from flask import Flask, render_template, request, g
-from typing import Dict, Union
+"""
+Flask app
+"""
+from flask import (
+    Flask,
+    render_template,
+    request,
+    g
+)
 from flask_babel import Babel
 
 
-class Config:
-    """babel config file"""
+class Config(object):
+    """
+    Configuration for Babel
+    """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -16,46 +24,53 @@ app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
 
-user = {
-        1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
-        2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
-        3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
-        4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
-        }
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
 
 
-def get_user() -> Union[Dict, None]:
-    """check if request has id and return its id value or None"""
-    login_as = request.args.get('login_as')
-    if login_as:
-        try:
-            return user.get(int(login_as))
-        except ValueError:
-            return None
+def get_user():
+    """
+    Returns a user dictionary or None if ID value can't be found
+    or if 'login_as' URL parameter was not found
+    """
+    id = request.args.get('login_as', None)
+    if id is not None and int(id) in users.keys():
+        return users.get(int(id))
     return None
 
 
 @app.before_request
 def before_request():
-    """calls get_user method and set it global on flask.g.user"""
+    """
+    Add user to flask.g if user is found
+    """
     user = get_user()
     g.user = user
 
 
 @babel.localeselector
 def get_locale():
-    """create a locale from request"""
-    locale = request.args.get('locale', '')
+    """
+    Select and return best language match based on supported languages
+    """
+    locale = request.args.get('locale')
     if locale in app.config['LANGUAGES']:
         return locale
-    return request.accept_languages.best_match(app.config["LANGUAGES"])
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
-def index():
-    """render 5-index.html"""
+def index() -> str:
+    """
+    Handles / route
+    """
     return render_template('5-index.html')
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(port="5000", host="0.0.0.0", debug=True)
