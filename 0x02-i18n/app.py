ef87b02e32b42c8fa36dbc4506eb34576842f2ad
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-07-19 16:52:55 +0300

    specify host and port for flask

--- 0x02-i18n/app.py
+++ 0x02-i18n/app.py
@@ -6,11 +6,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def index():
     """render 0-index.html template"""
     return render_template('0-index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=500, debug=True)
