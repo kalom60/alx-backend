a27214058be0b8fa48725d4a1e13177447e17b4e
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-07-19 16:50:18 +0300

    add docstring

--- 0x02-i18n/app.py
+++ 0x02-i18n/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""""""
+"""flask app"""
 from flask import Flask, render_template
 
 
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
-    """"""
+    """render 0-index.html template"""
     return render_template('0-index.html')
 
 
