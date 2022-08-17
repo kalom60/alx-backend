ab2824dcbb401385c565c639c43d91d53dd767b1
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-08-17 23:20:42 +0300

    Node Redis client publisher and subscriber

---
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,18 @@
+import redis from 'redis';
+
+const client = redis.createClient();
+
+client.on('connect', () => console.log('Redis client connected to the server'));
+client.on('error', (error) =>
+  console.log(`Redis client not connected to the server: ${error.message}`)
+);
+
+client.subscribe('holberton school channel');
+
+client.on('message', (channel, message) => {
+  if (channel === 'holberton school channel') console.log(message);
+  if (message === 'KILL_SERVER') {
+    client.unsubscribe();
+    client.quit();
+  }
+});
